3f037ca8362a9ad26fb13484537c1035300314ad
SAJJAD <122774486+snoou@users.noreply.github.com>
2025-04-14 12:06:07 +0330

    Update script.js

--- restaurant/script/script.js
+++ restaurant/script/script.js
@@ -1,9 +1,22 @@
-var swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+const slide = document.querySelectorAll(".slide-1");
+const nextBtn = document.querySelector(".swiper-button-next");
+const prevBtn = document.querySelector(".swiper-button-prev");
+
+let count = 0;
+
+function showSlide(index) {
+  slide.forEach((element) => {
+    element.classList.remove("active");
   });
+  slide[index].classList.add("active");
+}
+
+nextBtn.addEventListener('click', function(){
+  count = (count + 1) % slide.length;
+  showSlide(count)
+})
+
+prevBtn.addEventListener('click' , function(){
+ count =  (count - 1 + slide.length) % slide.length;
+ showSlide(count)
+})
